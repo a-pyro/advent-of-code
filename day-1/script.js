d7d458d701de1ff40ef9e0f2197879764756b5af
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-12-01 19:38:30 +0100

    part-2 solved

--- day-1/script.js
+++ day-1/script.js
@@ -30,4 +30,29 @@ const findEntries = (entries) => {
     }
 };
 
-console.log(findEntries(arrayInput));
+// console.log(findEntries(arrayInput));
+
+/* 
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+
+*/
+
+const findEntries2 = (entries) => {
+    for (let index = 0; index < entries.length; index++) {
+        const num1 = entries[index];
+        for (let index2 = 1; index2 < entries.length; index2++) {
+            const num2 = entries[index2];
+            for (let k = 2; k < entries.length; k++) {
+                const num3 = entries[k];
+                if (num1 + num2 + num3 === 2020) return num1 * num2 * num3;
+            }
+        }
+    }
+};
+
+console.log(findEntries2(arrayInput));
