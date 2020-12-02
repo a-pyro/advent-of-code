54e8f5894277b02d6e08e98523c40f4a405bd17b
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-12-02 12:13:38 +0100

    part-2-solved

--- day-2/script.js
+++ day-2/script.js
@@ -1035,21 +1035,45 @@ const passwords = puzzleInput.split('\n').map((el) => {
     };
 });
 
-console.log(passwords[0]);
-
 const validPasswords = (passArray) => {
-    let validCounter = 0;
+    let validCounterPolicy1 = 0;
+    let validCounterPolicy2 = 0;
     passArray.forEach((object) => {
         const { minChar, maxChar, char, password } = object;
         // console.log(minChar, maxChar, char, password);
         let count = 0;
+        let count2 = 0;
         for (const letter of password) {
             if (letter === char) count++;
         }
 
-        if (count >= minChar && count <= maxChar) validCounter++;
+        if (count >= minChar && count <= maxChar) validCounterPolicy1++;
+
+        // second part
+        if (password.charAt(minChar - 1) === char) count2++;
+        if (password.charAt(maxChar - 1) === char) count2++;
+
+        if (count2 === 1) validCounterPolicy2++;
     });
-    return validCounter;
+
+    return [validCounterPolicy1, validCounterPolicy2];
 };
 
 // console.log(validPasswords(passwords));
+console.log(validPasswords(passwords));
+
+/* 
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
+*/
