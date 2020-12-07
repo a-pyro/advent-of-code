022f773e72a0bea5459d0cf36d71f5e604b35cc7
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-12-07 16:22:13 +0100

    solve day-6

--- day-6/backup.js
+++ day-6/backup.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log('MerryXmas');
+console.log('Hello handsome : )');
 // console.log(input);
 const answers = input.split('\n');
 
@@ -16,7 +16,7 @@ for (let i = 0; i < answers.length; i++) {
         temp = [];
         continue;
     }
-    temp.push(element);
+    temp += element;
     if (i === answers.length - 1) {
         groupsAnswers.push(temp);
     }
@@ -29,34 +29,14 @@ let yesCounter = 0;
 
 const sets = [];
 
-for (const group of groupsAnswers.map((arr) => arr.join('').split(''))) {
+for (const group of groupsAnswers) {
     const uniqueAnswers = new Set([...group]);
     yesCounter += uniqueAnswers.size;
     sets.push(uniqueAnswers); //needed to try soluction with reduce
 }
 
-// console.log(yesCounter);
+console.log(yesCounter);
 
 /* const reduced = sets.reduce((ac, cv) => ac + cv.size, 0);
 console.log(reduced);
  */
-
-//  part 2
-
-console.log(sets);
-let part2counter = 0;
-
-for (let i = 0; i < sets.length; i++) {
-    const uniqueGroupAnswers = sets[i];
-
-    const currentGroup = groupsAnswers[i];
-    for (const uniqueAnswer of uniqueGroupAnswers) {
-        let counter = 0;
-        currentGroup.forEach((string) => {
-            if (string.includes(uniqueAnswer)) counter++;
-        });
-        if (counter === currentGroup.length) part2counter++;
-    }
-}
-
-console.log(part2counter); //yay!
