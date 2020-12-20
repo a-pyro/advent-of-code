689665804396c17cf7cb2289646324075ae21284
Ardi Germenji <ardigermenji@MacBook-Air-di-Ardi.local>
2020-12-20 22:48:07 +0100

    solve part 2

--- day-18/script.js
+++ day-18/script.js
@@ -109,11 +109,11 @@ const doHomeWorks = (homeworks) => {
 // PART 2 --------------------------------------------
 // with + that precede * in operator precedence, i need to apply solveParenthesis like algo, to first solve the additions
 const solveExpressionPart2 = (expression) => {
-    console.log(expression);
     const arrExp = expression
         .split(' ')
         .map((el) => (isNaN(parseFloat(el)) ? el : parseFloat(el)));
     console.log(arrExp);
+
     let accumulator = 0;
 
     let temp = [...arrExp];
@@ -125,12 +125,80 @@ const solveExpressionPart2 = (expression) => {
         console.log(temp);
     }
 
-    const result = arrExp
+    const result = temp
         .filter((el) => typeof el === 'number')
         .reduce((acc, curr) => acc * curr);
     console.log(result);
     return result;
 };
 
-solveExpressionPart2('1 + 2 * 3 + 4 * 5 + 6');
-solveExpressionPart2();
+// solveExpressionPart2('2 + 8 * 10 + 10 + 10 + 10 * 10'); //231
+
+const solveParenthesisPart2 = (expression) => {
+    // console.log(expression);
+    let openingIndex = 0;
+    let closingIndex = 0;
+    for (let i = 0; i < expression.length; i++) {
+        const element = expression[i];
+        if (element === '(') openingIndex = i;
+        if (element === ')') closingIndex = i + 1;
+        // when i find a closing parenthesis after an opening one
+        if (closingIndex) {
+            const exprBetweenPare = expression.slice(
+                openingIndex,
+                closingIndex
+            );
+            // console.log(exprBetweenPare);
+
+            // console.log(exprBetweenPare.slice(1, exprBetweenPare.length - 1)); withoutparen
+            const result = expression.replace(
+                exprBetweenPare,
+                `${solveExpressionPart2(
+                    exprBetweenPare.slice(1, exprBetweenPare.length - 1)
+                )}`
+            );
+            // console.log(result);
+            return result;
+        }
+    }
+};
+
+/* solveExpression(
+    solveParenthesis(
+        solveParenthesis('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
+    )
+); */
+
+const solveItAllPart2 = (expression) => {
+    let temp = expression;
+    let result = 0;
+    // console.log(temp);
+    if (!temp.includes('(')) {
+        // no more parenthesis to solve, solve expression
+        result = solveExpressionPart2(temp);
+    } else {
+        while (temp.includes('(')) {
+            temp = solveParenthesisPart2(temp);
+            // console.log(temp);
+        }
+        result = solveExpressionPart2(temp);
+    }
+
+    // console.log(result);
+    return result;
+};
+
+// solveItAll('8 * 3 + 9 + 3 * 4 * 3');
+// solveItAll('1 + (2 * 3) + (4 * (5 + 6))');
+// solveItAll('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2');
+// solveItAll('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))');
+
+const doHomeWorksPart2 = (homeworks) => {
+    const result = homeworks
+        .map((el) => solveItAllPart2(el))
+        .reduce((acc, curr) => acc + curr);
+    console.log(result);
+    return result;
+};
+
+doHomeWorksPart2(input);
